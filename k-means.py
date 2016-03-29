848f1de0fba004c0f82d7deb2c4519a561d471d0
bblu <gmwblu@gmail.com>
2016-03-29 20:19:11 +0800

    first k-means commit

---
+++ k-means.py
@@ -0,0 +1,99 @@
+#################################################  
+# kmeans: k-means cluster  
+# Author : bblu
+# Date   : 2016-03-29  
+# HomePage : http://blog.csdn.net/zouxy09  
+# Email  : gmwblu@gmail.cm 
+#################################################
+
+from numpy import *
+import time
+import matplotlib.pyplot as plt
+
+#calc distance^2
+def distance2(vec1,vec2):
+    return sum(power(vec2-vec1,2))
+
+#init centroids
+def initCentroids(dataSet, k):
+    numSamples, dim = dataSet.shape
+    centroids = zeros((k,dim))
+    for i in range(k):
+        index = int(random.uniform(0, numSamples))
+        centroids[i, :] = dataSet[index, :]
+    return centroids
+
+
+#k-means cluster
+def kmeans(dataSet, k):
+    numSamples = dataSet.shape[0]
+    clusterAssment = mat(zeros((numSamples, 2)))
+    clusterChanged = True
+
+    centroids = initCentroids(dataSet,k)
+
+    while clusterChanged:
+        clusterChanged = False
+
+        for i in xrange(numSamples):
+            minDist = 999999999999.9
+            minIndex = 0
+
+            for j in range(k):
+                dist = distance2(centroids[j, :], dataSet[i, :])
+                if dist < minDist:
+                    minDist = dist
+                    minIndex = j
+            if clusterAssment[i, 0] !=minIndex:
+                print "change index [%d]->[%d]" %(clusterAssment[i, 0],minIndex)
+                clusterChanged = True
+                clusterAssment[i, :] = minIndex, minDist
+        for j in range(k):
+            ptsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == j)[0]]
+            centroids[j, :] = mean(ptsInCluster, axis = 0)
+
+    print 'cluster over!'
+    return centroids, clusterAssment
+
+#show cluster result
+def showCluster(dataSet, k, centroids, clusterAssment):
+    numSamples, dim = dataSet.shap
+    if dim != 2:
+        print "sorry! dimension is not 2!"
+        return 1
+    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+
+    if k > len(mark):
+        print "k is too large!"
+        return 1
+
+    for i in xrange(numSamples):
+        markIndex = int(clusterAssment[i, 0])
+        plt.plot(dataset[i, 0], dataSet[i, 1], mark[markindex])
+
+    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
+
+    for i in range(k):
+        plt.plot(centroids[i,0], centroids[i, 1], mark[i], markersize = 12)
+
+    plt.show()
+    
+                        
+
+print 'step 1: load data...'
+
+dataSet = []
+
+fileIn = open('/home/bblu/python/testpts.txt')
+for line in fileIn.readlines():
+    pt = line.strip().split(' ')
+    dataSet.append([float(pt[0]), float(pt[1])])
+
+
+print 'step 2: clustering data...'
+
+dataSet = mat(dataSet)
+k = 4
+centroids, clusterAssment = kmeans(dataSet, k)
+
+showCluster(dataSet, k, centroids, clusterAssment)
