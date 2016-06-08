3ef07b2e7bd7c86c3f56573abb26cf4998185664
bblu <gmwblu@gmail.com>
2016-06-08 14:15:23 +0800

    sum function questions?

--- cookbook/1/1.1.py
+++ cookbook/1/1.1.py
@@ -22,6 +22,11 @@ print(middle)
 name,*_,(year,*_) = data
 print('1.2 get year in data item year = %d' % year)
 
+s1 = sum((1,2,3,4))
+s1 = sum([1,2,3,4])
+print('what is the different between s1 and s2?')
+
+
 def avg(marks):
     sum=0
     for m in marks:
