81cb56de60c0e58bde8537f3ab83cf81014049bf
wenbo.Lu <gmwblu@gmail.com>
2016-12-13 19:23:28 +0800

    function over

--- gis/cylindricalProjection.py
+++ gis/cylindricalProjection.py
@@ -8,13 +8,20 @@ from PIL import Image
 R=38.0
 r=500.0
 
-srcPic = Image.open('centrum.jpg')
+srcPic = Image.open('prjNT.jpg')
+#srcPic = Image.open('centrum.jpg')
 srcPixWidth,srcPixHeight=srcPic.size
-print "srcPixWidth =",srcPixWidth,"srcPixHeight=",srcPixHeight #280x50
-srcPixHalfWidth=(srcPixWidth+1)/2
-
+print "srcPixSize = (",srcPixWidth,",",srcPixHeight,')' #280x50
+srcPixHalfWidth=(srcPixWidth+1)/2.0
+if (srcPixWidth % 2) == 0:
+    srcPixHalfWidth=srcPixWidth/2.0
+    
+srcPixHalfHeight = (srcPixHeight+1)/2.0
+if (srcPixHeight % 2) == 0:
+    srcPixHalfHeight = srcPixHeight/2.0
+print "srcPixhalfSize = (",srcPixHalfWidth,",",srcPixHalfHeight,')' #280x50
 betaMax=math.asin(R/(R+r))
-print "betaMax =",betaMax,"degMax=", betaMax*180/math.pi
+#print "betaMax =",betaMax,"degMax=", betaMax*180/math.pi
 alphaMax=math.pi/2-betaMax
 print "alphaMax =",alphaMax,"degMax=", alphaMax*180/math.pi
 arcMax = alphaMax*R
@@ -22,7 +29,7 @@ print " arcMax = ",alphaMax,'*',R,'=',arcMax,'mm'
 
 srcHalfWidth=r*math.tan(betaMax)
 srcWidth = srcHalfWidth * 2
-print "srcHalfWidth=",srcHalfWidth, 'srcWidth =',srcWidth,'mm'
+#print "srcHalfWidth=",srcHalfWidth, 'srcWidth =',srcWidth,'mm'
 scalePermm = srcPixWidth/srcWidth 
 print "scalePermm = ",srcPixWidth,'/',srcWidth,'=',scalePermm,"pix/mm"
 pixR=R*scalePermm
@@ -30,13 +37,9 @@ pixr=r*scalePermm
 print "pixR =",pixR,"pixr =",pixr
 objHalfPixWidth = (int)(math.ceil(arcMax*scalePermm))
 objPixWidth = objHalfPixWidth * 2+1
-print "objPixWidth =",objPixWidth,'pix halfWidth =',objHalfPixWidth
-srcHalfPixHeight = (srcPixHeight+1)/2
 objPixHeight = srcPixHeight
-print "objPixHeight =",objPixHeight
+print "objPixSize =(",objPixWidth,objPixHeight,')'
 objHalfPixHeight = (objPixHeight+1)/2
-objPic = Image.new('RGB',[objPixWidth,objPixHeight])
-pixels = objPic.load()
 
 last=0.0
 def getPrjX(x):
@@ -60,54 +63,33 @@ def getPrjX(x):
         #print w,last-prjx,srcPixHalfWidth - prjx,arc,beta
         return srcPixHalfWidth + prjx
 
-#Ellipse
-effectR = objHalfPixWidth-124
-alphaVert = math.atan(srcPixHeight/(2*pixr))
-b = effectR * math.sin(alphaVert)
-a = effectR
-print "Ellipse: a =",a,"b =",b
-
-
-def getPrjY(fy,y):
-    global srcHalfPixHeight,b
-    delta = (b-fy)*(1-y/srcHalfPixHeight)
-    return y + delta
-
-if False:
-    srcHalfPixHeight=50.0
-    srcPixHalfWidth=50.0
-    b=10.0
-    a=50.0
-    for w in range(1,99):
-        for h in range(1,99):
-            prjY=getPrjY(w,h)
-
     
 def getInt(v1,v2,v3,v4,w1,w2,w3,w4):
     return int(math.floor(v1*w1+v2*w2+v3*w3+v4*w4))
 
-def getDLPix(x,y):
-    global srcPic
-    x0=math.floor(x)
-    x1=x0+1
-    r0,g0,b0 = srcPic.getpixel((x0,y))
-    r1,g1,b1 = srcPic.getpixel((x1,y))
-    r=int(math.floor(r0*(x-x0)+r1*(x1-x0)))
-    g=int(math.floor(g0*(x-x0)+g1*(x1-x0)))
-    b=int(math.floor(b0*(x-x0)+b1*(x1-x0)))
-    return (r,g,b)
+def getNTPix(x,y):
+    global srcPic,srcPixWidth,srcPixHeight
+    w=int(round(x))
+    h=int(round(y))
+    if w == srcPixWidth:
+        w = srcPixWidth-1
+    if h == srcPixHeight:
+        h = srcPixHeight-1
+    return srcPic.getpixel((w,h))
 
-def getDLPix2(x,y):
+def getDLPix(x,y):
     global srcPic,srcPixWidth,srcPixHeight
     if x+1 > srcPixWidth:
         x = srcPixWidth-1
     if y+1 > srcPixHeight:
         y = srcPixHeight-1
     cy=math.ceil(y)
-    if(cy<1):
+    if cy<1:
         cy=1
     fy=cy-1
     cx=math.ceil(x)
+    if cx<1:
+        cx=1
     fx=cx-1
     
     #print fx,x,cx,fy,y,cy
@@ -128,17 +110,69 @@ def getDLPix2(x,y):
     else:#print x,y
         return srcPic.getpixel((int(x),int(y)))
 
-for w in range(objPixWidth):
-    prjX=getPrjX(w)
-    fx = prjX-srcPixHalfWidth
-    fx2 = (fx/a) * (fx/a)
-    fy = b * math.sqrt(1-fx2)
-    if(prjX>=0 and prjX<srcPixWidth):
-        #print w,prjX
-        for h in range(0, objPixHeight):
-            prjY=getPrjY(fy,h)
-            #if w==300:
-#                print w,h,prjY
-            pixels[w,h]=getDLPix2(prjX, prjY)
+def getPrjY(fy,y):
+    global srcPixHalfHeight,b
+    delta = (b-fy)*(1-y/srcPixHalfHeight)
+    #print delta,'=',b-fy,'*',1-y/srcPixHalfHeight
+    return y + delta
 
-objPic.save('obj.jpg','JPEG')
+
+#Ellipse
+alphaVert = math.atan(srcPixHeight/(2*pixr))
+effectR = srcPixHalfWidth
+a = effectR
+b = a * math.sin(alphaVert)
+tranWb = 0
+for w in range(int(srcPixHalfWidth)):
+        prjX=getPrjX(w)
+        if prjX>=0:
+            tranWb=w
+            break
+print 'tranWb =',tranWb
+out=3
+if out==1:
+    #b = 90
+    objPic = Image.new('RGB',[srcPixWidth,srcPixHeight])
+    pixels = objPic.load()
+    for w in range(srcPixWidth):
+        fx = w-srcPixHalfWidth
+        fx2 = (fx/a) * (fx/a)
+        fy = b * math.sqrt(1-fx2)
+        for h in range(0, srcPixHeight):
+            delta = (b-fy)*(1-h/srcPixHalfHeight)
+            prjY= h + delta
+            pixels[w,h]=getNTPix(w, prjY)
+    objPic.save('prjVert.jpg','JPEG')
+
+elif out==2:
+    objPic = Image.new('RGB',[objPixWidth,objPixHeight])
+    pixels = objPic.load()
+    for w in range(objPixWidth):
+        prjX=getPrjX(w)        
+        if prjX>=0 and prjX<srcPixWidth:
+            for h in range(0, objPixHeight):
+                #print fx,fy, w,'->',prjX,h,'->',prjY,'delta',
+                pixels[w,h]=getNTPix(prjX, h)
+    objPic.save('prjHori.jpg','JPEG')
+else:
+    tranW = int(objPixWidth-tranWb*2)+1
+    print tranWb,tranW,objPixWidth
+    objPic = Image.new('RGB',[tranW,objPixHeight])    
+    pixels = objPic.load()
+    for w in range(tranWb,tranWb+tranW):
+        prjX=getPrjX(w)
+        if prjX>=0 and prjX<srcPixWidth:
+            fx = prjX-srcPixHalfWidth
+            fx2 = (fx/a) * (fx/a)
+            fy = b * math.sqrt(1-fx2)
+            if prjX>=0 and prjX<srcPixWidth:
+                fy = b
+                fx = prjX-effectR
+                if fx>-a and fx < a:
+                    fx2 = (fx/a) * (fx/a)
+                    fy = b * math.sqrt(1-fx2)
+                for h in range(0, objPixHeight):
+                    delta = (b-fy)*(1-h/srcPixHalfHeight)
+                    prjY= h + delta
+                    pixels[w-tranWb,h]=getNTPix(prjX, prjY)
+    objPic.save('prjAll.jpg','JPEG')
