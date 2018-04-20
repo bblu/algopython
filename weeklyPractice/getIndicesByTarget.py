e2b758480c746c0c10dd2aa8dac0045ec2b90116
bblu <gmwblu@gmail.com>
2018-04-20 22:29:10 +0800

    v2

--- weeklyPractice/getIndicesByTarget.py
+++ weeklyPractice/getIndicesByTarget.py
@@ -2,19 +2,24 @@
 
 class Solution:
     def getIndicesByTarget(self,nums,tar):
-        diff = -1
-        indices = []
-        for i,v in enumerate(nums):
-            if v < tar:
-                indices.append(i)
-                tar = tar - v 
-            elif v == tar:
-                indices.append(i)
-                return indices
+        for base in range(0,len(nums)):
+            p0,p1 = -1,-1
+            print '|-deal with base=%s' % base
+            subNums = nums[base:] 
+            for i,v in enumerate(subNums):
+                if p0 < 0 and v < tar:
+                    p0 = base + i
+                    print '|--p0=%s'%p0
+                    p1 = tar - v 
+                elif v == p1:
+                    print '|-find index:base=%s' % base
+                    return [p0, base+i]
+            print '|-not find index:base=%s' % base
+        return [p0,p1]
 
 if __name__ == '__main__':
     mySolution = Solution()
-    nums = [2,10,7,5,8]
+    nums = [1,2,10,7,5,8]
     tar = 7
     indices = mySolution.getIndicesByTarget(nums,tar)
     if len(indices) == 2:
