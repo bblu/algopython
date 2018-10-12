59af0c20e1aea9c5a518d863d4ad8fb1fb9e0f1d
wenbo.Lu <gmwblu@gmail.com>
2018-10-12 14:44:01 +0800

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -6,6 +6,8 @@
 # Email  : gmwblu@gmail.cm 
 #################################################
 #DDS-QSW-D-G-16-WT105
+#DDS-LTHF-D-H-31-ST173
+
 from numpy import *
 import time
 import matplotlib.pyplot as plt
